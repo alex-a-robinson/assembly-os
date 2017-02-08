3d227a7f903aac5b8e62f9d78d169ceecee7eb6a
begly <service.alex@begly.co.uk>
2017-02-08 00:22:14 +0000

    section 1 complete

--- question/kernel/hilevel.h
+++ question/kernel/hilevel.h
@@ -6,10 +6,71 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
+
+// Include functionality relating to the platform.
+
+#include   "GIC.h"
+#include "PL011.h"
+#include "SP804.h"
 
 // Include functionality relating to the   kernel.
 
 #include "lolevel.h"
 #include     "int.h"
+
+/* The kernel source code is made simpler via three type definitions:
+ *
+ * - a type that captures a Process IDentifier (PID), which is really
+ *   just an integer,
+ * - a type that captures each component of an execution context (i.e.,
+ *   processor state) in a compatible order wrt. the low-level handler
+ *   preservation and restoration prologue and epilogue, and
+ * - a type that captures a process PCB.
+ */
+
+typedef int pid_t;
+
+typedef struct {
+  uint32_t cpsr, pc, gpr[ 13 ], sp, lr;
+} ctx_t;
+
+typedef struct {
+  pid_t pid;
+  ctx_t ctx;
+  int complete;
+} pcb_t;
+
+/* The definitions below capture symbolic constants within these classes:
+ *
+ * 1. system call identifiers (i.e., the constant used by a system call
+ *    to specify which action the kernel should take),
+ * 2. signal identifiers (as used by the kill system call),
+ * 3. status codes for exit,
+ * 4. standard file descriptors (e.g., for read and write system calls),
+ * 5. platform-specific constants, which may need calibration (wrt. the
+ *    underlying hardware QEMU is executed on).
+ *
+ * They don't *precisely* match the standard C library, but are intended
+ * to act as a limited model of similar concepts.
+ */
+
+#define SYS_YIELD     ( 0x00 )
+#define SYS_WRITE     ( 0x01 )
+#define SYS_READ      ( 0x02 )
+#define SYS_FORK      ( 0x03 )
+#define SYS_EXIT      ( 0x04 )
+#define SYS_EXEC      ( 0x05 )
+#define SYS_KILL      ( 0x06 )
+
+#define SIG_TERM      ( 0x00 )
+#define SIG_QUIT      ( 0x01 )
+
+#define EXIT_SUCCESS  ( 0 )
+#define EXIT_FAILURE  ( 1 )
+
+#define  STDIN_FILENO ( 0 )
+#define STDOUT_FILENO ( 1 )
+#define STDERR_FILENO ( 2 )
 
 #endif
