abb1f29a58945c8757faef13276231277d9c17a4
begly <service.alex@begly.co.uk>
2017-02-21 15:19:09 +0000

    update todo

--- question/kernel/fs.c
+++ question/kernel/fs.c
@@ -2,25 +2,14 @@
 
 /*
 TODO:
-  - Check for existance of super block, create if dosen't exist
-  - implement read by direct inode number first
-  - Create super block layout
-    - size of disk, number of inodes, number of data blocks, where inodes start, where data blocks start
-  - Implement inode structure
-    - inode number
-    - TYPE = "FILE" or "Directory" or "UNALLOCATED"
-    - size = file size in bytes
-    - number of blocks allocated
-    - creation time, modification time, ...
-    - 6 Direct Block Pointers which point to specific data blocks
   - Implement Data blocks
-  - In memory inode lookup table
   - Implement system calls
     - open, read, write, close
     - fstat = stat with file descriptor id
     - stat = file info from path
   - Implement cat and wc and load a program into memroy
   - Write root files
+  - Read and write a byte at a time with inodes
 */
 
 // Returns ceil of number of blocks bytes bytes occupies
