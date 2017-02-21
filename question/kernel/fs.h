56e3439d50278bc8b4208789097670e48535eee5
begly <service.alex@begly.co.uk>
2017-02-21 12:26:07 +0000

    superblock type

--- question/kernel/fs.h
+++ question/kernel/fs.h
@@ -6,7 +6,28 @@
 #include <stdint.h>
 #include <string.h>
 
+// TODO Should these be hard coded?
+#define DISK_BLOCK_NUM 65536
+#define DISK_BLOCK_LEN 16
+
+// Inode constatnts
 #define MAX_INODE_BLOCKS 6
+
+// Super Block constants
+#define NUM_OF_INODES ((int) (DISK_BLOCK_NUM) / (MAX_INODE_BLOCKS))
+
+// inode types
+#define INODE_UNALLOCATED 0
+#define INODE_FILE 1
+#define INODE_DIRECTORY 2
+
+typedef struct {
+    int disk_block_num;
+    int disk_block_length;
+    int inode_num;
+    void* inode_start;
+    void* data_block_start;
+} superblock_t;
 
 typedef struct {
     int id;
